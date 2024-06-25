6d153d602b666536a39a648772d34d8365fd3b14
Julien Nicole <j.nicole@e-nnov.fr>
2024-06-25 22:56:46 +0200

    creates /cv page

--- src/webapp/app/page.tsx
+++ src/webapp/app/page.tsx
@@ -1,15 +1,5 @@
-import { CV } from "@/components/cv";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: 'Développeur fullstack .net javascript à Rennes - Julien Nicole',
-    description: "Développeur sénior depuis 20 ans, je vous propose mes services en développement logiciel sur les technologies .net et Javascript (React)",
-  }
+import { RedirectType, redirect } from 'next/navigation';
 
 export default function Home() {
-    return (
-        <main className="w-screen h-screen print:w-full print:h-full flex items-center justify-center">
-            <CV />
-        </main>
-    )
+    redirect('/cv', RedirectType.replace)
 }
