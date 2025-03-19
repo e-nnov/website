157bc47c60b3044e9d2337ac2aaa6126c5e605a3
Julien Nicole <j.nicole@e-nnov.fr>
2025-03-19 13:02:13 +0100

    adds canonical

--- src/webapp/app/cv/page.tsx
+++ src/webapp/app/cv/page.tsx
@@ -4,6 +4,9 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
     title: 'Développeur fullstack .net javascript à Rennes - Julien Nicole',
     description: "Développeur sénior depuis 20 ans, je vous propose mes services en développement logiciel sur les technologies .net et Javascript (React)",
+    alternates:{
+        canonical: "https://www.e-nnov.fr/cv"
+    }
   }
 
 export default function Resume() {
